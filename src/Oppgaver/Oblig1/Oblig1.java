6166c39939733a4badb651f4ca9135df7b4643a3
Iver2101 <54900795+Iver2101@users.noreply.github.com>
2020-09-08 18:47:38 +0200

    oppg. 4

--- src/Oppgaver/Oblig1/Oblig1.java
+++ src/Oppgaver/Oblig1/Oblig1.java
@@ -56,8 +56,63 @@ public class Oblig1 {
     }
 
     ///// Oppgave 4 //////////////////////////////////////
+
+    private static int parter0(int[] a, int v, int h, int skilleverdi)
+    {
+        while (true)                                  // stopper når v > h
+        {
+            while (v <= h && a[v] < skilleverdi) v++;   // h er stoppverdi for v
+            while (v <= h && a[h] >= skilleverdi) h--;  // v er stoppverdi for h
+
+            if (v < h) Oblig1Test.bytt(a,v++,h--);                 // bytter om a[v] og a[h]
+            else  return v;  // a[v] er nåden første som ikke er mindre enn skilleverdi
+        }
+    }
+
+    private static int sParter0(int[] a, int v, int h, int indeks)
+    {
+        Oblig1Test.bytt(a, indeks, h);           // skilleverdi a[indeks] flyttes bakerst
+        int pos = parter0(a, v, h - 1, a[h]);  // partisjonerer a[v:h - 1]
+        Oblig1Test.bytt(a, pos, h);              // bytter for å få skilleverdien på rett plass
+        return pos;                   // returnerer posisjonen til skilleverdien
+    }
+
+    public static void quicksort(int[] a, int start, int end) {
+
+        if(start < end) {
+            int k = sParter0(a,start,end,(end + start) / 2);
+            quicksort(a,start,k-1);
+            quicksort(a,k + 1,end);
+
+        }
+    }
+
     public static void delsortering(int[] a) {
-        throw new UnsupportedOperationException();
+        if (a.length == 0) {
+            return;  //Avbryter metoden viss arrayet er tomt.
+        }
+        boolean odds = true; // Lager boolean for å sjekke om arrayet bare inneholder oddetall
+        int left = 0;
+        int right = a.length - 1;
+        while(right>left) {
+
+            while (a[left] % 2 != 0 && right > left) { //Flytter venstre peker til høgre viss det er et oddetall
+                left++;
+            }
+            while (a[right] % 2 == 0 && right > left) { //Flytter høgre peker til venstre viss det er et partall
+                right--;
+                odds = false; // Arrayet inneholder ikke bare oddetall
+            }
+            Oblig1Test.bytt(a,left,right);      // bytter om elementene de forskjellige pekerene tilhører
+        }
+        int end = left-1;
+        if(odds) {
+            end = left;
+        }
+        quicksort(a,0,end); //Sorterer oddetallene
+        if(!odds) {
+            quicksort(a, left, a.length - 1); //sorterer partallene
+        }
     }
 
     ///// Oppgave 5 //////////////////////////////////////
